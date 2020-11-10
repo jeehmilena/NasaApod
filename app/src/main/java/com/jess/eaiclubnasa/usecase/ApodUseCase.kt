7c2a54367c60aec046809d47ddf23f8825b7d998
jeehmilena <jessicamilenacorrea@gmail.com>
2020-11-10 15:07:27 -0300

    inicio da implementação logica

---
+++ app/src/main/java/com/jess/eaiclubnasa/usecase/ApodUseCase.kt
@@ -0,0 +1,38 @@
+package com.jess.eaiclubnasa.usecase
+
+import com.jess.eaiclubnasa.model.ApodResult
+import com.jess.eaiclubnasa.repository.ApodRepository
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.Calendar
+
+class ApodUseCase {
+    private val repository = ApodRepository()
+
+    private fun getListDate(date: String): List<String> {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val currentCalendar = Calendar.getInstance()
+        val listDate = arrayListOf<String>()
+        val data = dateFormat.parse(date)
+        currentCalendar.time = data
+
+        for (i in 1..5) {
+            currentCalendar.add(Calendar.DATE, -1)
+            val toDate = dateFormat.format(currentCalendar.time)
+            listDate.add(toDate)
+        }
+        return listDate
+    }
+
+    suspend fun getApodList(date: String): List<ApodResult> {
+        val listApod = mutableListOf<ApodResult>()
+        val listDate = getListDate(date)
+
+        listDate.forEach {
+            val apod = repository.getApod(it)
+            listApod.add(apod)
+        }
+
+        return listApod
+    }
+}
--- app/src/main/java/com/jess/eaiclubnasa/view/HomeActivity.kt
+++ app/src/main/java/com/jess/eaiclubnasa/view/HomeActivity.kt
@@ -1,12 +1,13 @@
 package com.jess.eaiclubnasa.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.jess.eaiclubnasa.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
     }
 }
