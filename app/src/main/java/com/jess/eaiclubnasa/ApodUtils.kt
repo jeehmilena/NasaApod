66af79632ffc57e433f7c7e22e61f2cf196116f9
jeehmilena <jessicamilenacorrea@gmail.com>
2020-11-11 00:47:16 -0300

    Ajuste no ApodUtils.kt

--- app/src/main/java/com/jess/eaiclubnasa/ApodUtils.kt
+++ app/src/main/java/com/jess/eaiclubnasa/ApodUtils.kt
@@ -11,23 +11,30 @@ object ApodUtils {
         val dateFormat = SimpleDateFormat(DATE_PATTERN_YEAR_MONTH_DAY, Locale.getDefault())
         val currentCalendar = Calendar.getInstance()
         val newDate = dateFormat.parse(date)
-        currentCalendar.time = newDate
+        newDate?.let {
+            currentCalendar.time = it
 
-        for (i in 0..5) {
-            currentCalendar.add(Calendar.DATE, -1)
-            val toDate = dateFormat.format(currentCalendar.time)
-            listDate.add(toDate)
+            for (i in 0..5) {
+                currentCalendar.add(Calendar.DATE, -1)
+                val toDate = dateFormat.format(currentCalendar.time)
+                listDate.add(toDate)
+            }
         }
         return listDate
     }
 
-    fun getCurrentDate() =
+    fun getCurrentDate(): String =
         SimpleDateFormat(DATE_PATTERN_YEAR_MONTH_DAY, Locale.getDefault()).format(Date())
 
     fun formatDate(date: String): String {
-        val dateFormat = SimpleDateFormat(DATE_PATTERN_YEAR_MONTH_DAY, Locale.getDefault())
-        val newDate = dateFormat.parse(date)
+        val format = SimpleDateFormat(DATE_PATTERN_YEAR_MONTH_DAY, Locale.getDefault())
+        val newDate = format.parse(date)
         val dateToFormat = SimpleDateFormat(DATE_PATTERN_DAY_MONTH_YEAR, Locale.getDefault())
-        return dateToFormat.format(newDate)
+        var dateFormated = ""
+
+        newDate?.let {
+            dateFormated = dateToFormat.format(it)
+        }
+        return dateFormated
     }
 }
