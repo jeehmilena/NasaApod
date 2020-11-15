7c9ff58540dae38d48fcd7b3968e4531895a6ede
jeehmilena <jessicamilenacorrea@gmail.com>
2020-11-14 23:19:30 -0300

    desafio android

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/jess/eaiclubnasa/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.jess.eaiclubnasa.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.jess.eaiclubnasa.test";
+  public static final String BUILD_TYPE = "debug";
+}
